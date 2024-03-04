4d6a8fa8c9cefe2c8b5f71a222b83916aebb9e83
kalpana123-1 <kalpanatamatam1822000@gmail.com>
2024-03-04 17:13:27 +0530

    Day-1 learning update

--- practice1.py
+++ practice1.py
@@ -1,6 +1,31 @@
 from google.cloud import bigquery
+
+# Create a "Client" object
 client = bigquery.Client()
 
 
+# Construct a reference to the "hacker_news" dataset
 dataset_ref = client.dataset("hacker_news", project="bigquery-public-data")
 dataset = client.get_dataset(dataset_ref)
+
+# List of all the tables
+tables = list(client.list_tables(dataset))
+for table in tables:  
+    print(table.table_id)
+
+
+# Fecth full table
+table_ref = dataset_ref.table("full")
+table = client.get_table(table_ref)
+
+
+# Print information on all the columns in the full table
+table.schema
+
+
+# First five lines of the full table
+client.list_rows(table, max_results=5).to_dataframe()
+
+
+# First five entries in the by column of the full table
+client.list_rows(table, selected_fields=table.schema[:1], max_results=5).to_dataframe()
